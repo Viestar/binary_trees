c9ca6a87325b5749a88fe4957815e66e8584ee9b
Viestar <m.silverbuckson@gmail.com>
2023-08-02 16:51:19 +0300

    Task 19 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -3,6 +3,7 @@
 /* Prototypes */
 binary_tree_t *peep_nodes(const binary_tree_t *first,
 						  const binary_tree_t *second);
+size_t binary_tree_local_depth(const binary_tree_t *tree);
 
 /**
  * binary_trees_ancestor - finds the lowest common ancestor of the two nodes.
@@ -15,42 +16,41 @@ binary_tree_t *peep_nodes(const binary_tree_t *first,
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 									 const binary_tree_t *second)
 {
-	binary_tree_t *local_first = first;
-	binary_tree_t *local_second = second;
+
 	binary_tree_t *lowest_common;
 	size_t depth_first, depth_second;
 
 	/* Checking if empty node are passed */
-	if (!local_first || !local_second)
+	if (!first || !second)
 		return (NULL);
 
 	/* In case the passed nodes are identical */
-	if (local_first == local_second)
-		return (local_first);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
 	/* Incase passed nodes have the same parents */
-	if (local_first->parent == local_second->parent)
-		/* return (lowest_common = peep_nodes(local_first, local_second)); */
-		return (local_second->parent);
+	if (first->parent == second->parent)
+		/* return (lowest_common = peep_nodes(first, second)); */
+		return ((binary_tree_t *)second->parent);
 
 	/* Incase second node same to first's parent */
-	if (local_second == local_first->parent)
-		return (local_first->parent);
+	if (second == first->parent)
+		return ((binary_tree_t *)first->parent);
 
 	/* Incase first node same to second's parent */
-	if (local_first == local_second->parent)
-		return (local_first);
+	if (first == second->parent)
+		return ((binary_tree_t *)first);
 
 	/* Traversing through both nodes to get the depth */
-	for (depth_first = binary_tree_depth(local_first);
+	for (depth_first = binary_tree_local_depth(first);
 		 depth_first > 1; depth_first--)
-		local_first = local_first->parent;
-	for (depth_second = binary_tree_depth(local_second);
+		first = first->parent;
+	for (depth_second = binary_tree_local_depth(second);
 		 depth_second > 1; depth_second--)
-		local_second = local_second->parent;
+		second = second->parent;
 
-	if (local_first && local_second)
-		return (lowest_common = peep_nodes(local_first, local_second));
+	if (first && second)
+		return (lowest_common = peep_nodes(first, second));
 	else
 		return (NULL);
 }
@@ -65,22 +65,32 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 binary_tree_t *peep_nodes(const binary_tree_t *first,
 						  const binary_tree_t *second)
 {
-	binary_tree_t *local_first = first;
-	binary_tree_t *local_second = second;
 
 	/* In case the passed nodes are identical */
-	if (local_first == local_second)
-		return (local_first);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
 	/* Incase passed nodes have the same parents */
-	if (local_first->parent == local_second->parent)
-		return (local_second->parent);
+	if (first->parent == second->parent)
+		return ((binary_tree_t *)second->parent);
 
 	/* Incase second node same to first's parent */
-	if (local_second == local_first->parent)
-		return (local_first->parent);
+	if (second == first->parent)
+		return ((binary_tree_t *)first->parent);
 
 	/* Incase first node same to second's parent */
-	if (local_first == local_second->parent)
-		return (local_first);
+	if (first == second->parent)
+		return ((binary_tree_t *)first);
+
+	return (0);
+}
+
+/**
+ * depth - Measures node depth.
+ * @tree: A pointer to the node.
+ * Return: If tree is NULL, your function must return 0, else return the depth.
+ */
+size_t binary_tree_local_depth(const binary_tree_t *tree)
+{
+	return ((tree->parent != NULL) ? 1 + binary_tree_local_depth(tree->parent) : 0);
 }
